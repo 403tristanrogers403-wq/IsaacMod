f0aeb7046357ed531064de94189304f8d20fc3ca
Tristan-Rog <s242150@students.aie.edu.au>
2026-08-17 09:52:19 +0930

    Ansainsafnioasf
    asfdb;ou;adsf;oiagdi;ou

--- TBOI/main.lua
+++ TBOI/main.lua
@@ -1,7 +1,6 @@
 --Start of Mod
-local Mod = RegisterMod("Tech Transformation", 1)
+local Mod =  RegisterMod("TestMod123", 1)
 local game = Game()
-
 local testing = true
 local player = Isaac.GetPlayer(0)
 
@@ -18,15 +17,26 @@ function Mod:check()
 end
 
 if player:HasCollectible(TECHNOLOGY) and player:HasCollectible(BOTFLY) and player:HasCollectible(TWISTEDPAIR) then
-isTech = true
-Game():GetPlayer(0):ClearCostumes()
+    isTech = true
+    Isaac.DebugString("Player has all three items for the transformation")
+    Game():GetPlayer(0):ClearCostumes()
+    Game():GetPlayer(0):AddCostume(Isaac.GetCostumeIdByPath("gfx/characters/techfly.anm2"))
 end
 
+function Mod:onCache(player, cacheFlag)
+    if game:GetFrameCount() == 1 then
+  local isTech = false
+end
 if cacheFlag == CacheFlag.CACHE_DAMAGE then
     if isTech then
       player.Damage = player.Damage *1.25
+      Isaac.DebugString("Damage Given")
     end
   end
+
+
+isTech = false
+end
   function Mod:onUpdate()
 	-- SPAWN ITEMS FOR TESTING
 	if testing == true then
@@ -34,13 +44,8 @@ if cacheFlag == CacheFlag.CACHE_DAMAGE then
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TECHNOLOGY, Vector(295,300), Vector(0,0), nil)
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, BOTFLY, Vector(345,300), Vector(0,0), nil)
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, TWISTEDPAIR, Vector(395,300), Vector(0,0), nil)
+            Isaac.DebugString("Spawned Items for Testing")
     end
   end
 end
 
-
-Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, Mod.newRun)
-Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Mod.onCache)
-Mod:AddCallback(ModCallbacks.MC_POST_UPDATE, Mod.onUpdate)
-Mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, Mod.damage)
-Mod:AddCallback(ModCallbacks.MC_POST_UPDATE, Mod.check)
